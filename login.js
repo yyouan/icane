1db548837783af2821c44ae9ad251fe31bf4d2be
yyouan <35065849+yyouan@users.noreply.github.com>
2018-06-17 17:41:31 +0800

    all-group psql-fix-promise-1

--- login.js
+++ login.js
@@ -31,17 +31,18 @@ app.get('/data',datareceiver);
 */  
 
 //login message with recpt function:
-function record_id(id,email){
-    psql("UPDATE ACCOUNTS SET line_id=\'"+ id +"\' WHERE email=\'" + email +"\';");        
-}
+function record_dev_name(name,id,isgroup,email){
 
-function record_dev_name(name,isgroup,email){
-    psql("INSERT INTO ACCOUNTS (email) VALUES (\'"+ email +"\');");
-    psql("UPDATE ACCOUNTS SET times=\'"+ "0" +"\' WHERE email=\'" + email +"\';");
-    psql("UPDATE ACCOUNTS SET dev_name=\'"+ name +"\' WHERE email=\'" + email +"\';");
-    psql("UPDATE ACCOUNTS SET isgroup=\'"+ isgroup +"\' WHERE email=\'" + email +"\';");
-    psql("UPDATE ACCOUNTS SET last_call_time=\'\' WHERE email=\'" + email +"\';"); 
-    psql("UPDATE ACCOUNTS SET stepcount=0 WHERE email=\'" + email +"\';");    
+    psql("INSERT INTO ACCOUNTS (email) VALUES (\'"+ email +"\');").then(
+        res =>{
+            psql("UPDATE ACCOUNTS SET times=\'"+ "0" +"\' WHERE email=\'" + email +"\';");
+            psql("UPDATE ACCOUNTS SET dev_name=\'"+ name +"\' WHERE email=\'" + email +"\';");
+            psql("UPDATE ACCOUNTS SET isgroup=\'"+ isgroup +"\' WHERE email=\'" + email +"\';");
+            psql("UPDATE ACCOUNTS SET last_call_time=\'\' WHERE email=\'" + email +"\';"); 
+            psql("UPDATE ACCOUNTS SET stepcount=0 WHERE email=\'" + email +"\';");
+            psql("UPDATE ACCOUNTS SET line_id=\'"+ id +"\' WHERE email=\'" + email +"\';");  
+        }
+    );      
 }
 function create_dev_name(email,line_id){
         let google_url = 'https://docs.google.com/spreadsheets/d/1VWr1uoN0n9KD3h74G3P7HItf8-Hg2Pg9lN8ygJwQH7w/gviz/tq?tqx=out:html&tq=select%20*%20where%20E%20=%20%27';
@@ -72,13 +73,12 @@ function create_dev_name(email,line_id){
               console.log("dev_name:"+ dev_name);
               console.log("is_group:"+ isgroup);
               if(dev_name !="&nbsp" && isgroup !="&nbsp"){
-                record_dev_name(dev_name,isgroup,email);
-                record_id(line_id,email);
+                record_dev_name(dev_name,line_id,isgroup,email);                
               }else{
                 var req = post.events[0].message;
                 req.text ="您還沒填表單喔!";
                 replymessage([req]);
-              }              
+              }             
               
             }else{
               console.log(error);
@@ -93,11 +93,12 @@ pool.on('error', (err, client) => {
 })
   
 function psql(command){
-
+   
     let recpt =[];
     //while(is_conn_psql){console.log("(psql):pararell gate");};
     //if(!is_conn_psql){client.connect();is_conn_psql = true;}
     console.log("(psql):" + command );
+    var pool_promise = 
     pool.connect()
     .then(client=>{
         client.query(command)
@@ -108,13 +109,17 @@ function psql(command){
             }    
         }).catch(e => {client.release(); console.error("(psql):" + e.stack);}); 
     })
+    .then( res=> {
+        console.log( "(psql-query):"+ JSON.stringify(recpt));
+    })
     .catch(e => {
         client.release();
-        console.log("(psql):" + err.stack);        
-    });
-    console.log( "(psql-query):"+ JSON.stringify(recpt));
-    return recpt;
+        console.log("(psql):" + err.stack);
+        console.log( "(psql-query):"+ JSON.stringify(recpt));
+        return recpt;      
+    });    
     
+    return pool_promise;
 }
 
 function linebotParser(req ,res){
@@ -178,28 +183,35 @@ function linebotParser(req ,res){
         
         if (posttype == 'message'){
             
-            if(post.events[0].message.type == 'text'){             
+            if(post.events[0].message.type == 'text'){
                 
-                if(psql("SELECT * FROM ACCOUNTS WHERE line_id=\'" + line_id +"\';").length == 0)   
-                {
-                    var email = post.events[0].message.text;
-                    if( psql("SELECT * FROM ACCOUNTS WHERE email=\'" + email +"\';").length == 0 )
+                psql("SELECT * FROM ACCOUNTS WHERE line_id=\'" + line_id +"\';")
+                .then( recpt =>{
+                    if( recpt.length == 0)   
                     {
-                        create_dev_name(email,line_id);                        
-                        var req = post.events[0].message;
-                        req.text ="成功紀錄!";
-                        replymessage([req]);
+                        var email = post.events[0].message.text;
+                        psql("SELECT * FROM ACCOUNTS WHERE email=\'" + email +"\';").then(recpt=>{
+                            if( recpt.length == 0 )
+                            {
+                                create_dev_name(email,line_id);                        
+                                var req = post.events[0].message;
+                                req.text ="成功紀錄!";
+                                replymessage([req]);
+
+                            }else{
+                                var req = post.events[0].message;
+                                req.text ="這個郵件信箱註冊過了喔!";
+                                replymessage([req]);                        
+                            }
+                        });
 
                     }else{
                         var req = post.events[0].message;
-                        req.text ="這個郵件信箱註冊過了喔!";
-                        replymessage([req]);                        
-                    }
-                }else{
-                    var req = post.events[0].message;
-                    req.text ="您的回饋已經傳送給官方!";
-                    replymessage([req]);
-                }                
+                        req.text ="您的回饋已經傳送給官方!";
+                        replymessage([req]);
+                    }    
+                });                
+                            
             }
         }        
 
@@ -258,77 +270,89 @@ function datareceiver(req,res){
     }
 
     psql("UPDATE ACCOUNTS SET last_call_time=\'"+ JSON.stringify(time) +"\' WHERE dev_name=\'" + dev +"\';");
-    var stepcount = psql("SELECT stepcount FROM ACCOUNTS WHERE dev_name=\'" + dev +"\';").stepcount;
-    stepcount += parseInt(data.step);
-    psql("UPDATE ACCOUNTS SET stepcount="+ stepcount +" WHERE dev_name=\'" + dev +"\';");
+    psql("SELECT stepcount FROM ACCOUNTS WHERE dev_name=\'" + dev +"\';").then( recpt =>{
+        let stepcount = recpt.stepcount;
+        stepcount += parseInt(data.step);
+        psql("UPDATE ACCOUNTS SET stepcount="+ stepcount +" WHERE dev_name=\'" + dev +"\';");
+        return stepcount;
+    })
+    .then( stepcount =>{
 
-    var msg ={
-        "type": "bubble",
-        "header": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "一般通知訊息"
-            }
-          ]
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "角度狀態： "+ (data.ang=='0')?"Standing":"Lying",
-            },                
-            {
-                "type": "text",
-                "text": "是否拿著？ "+ (data.isactive=='0')?"沒拿":"拿著",
+        var msg ={
+            "type": "bubble",
+            "header": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "一般通知訊息"
+                }
+              ]
             },
-            {
-                "type": "text",
-                "text": "本日腳步數： "+ stepcount +"步",
-            }                
-          ]
-        }            
-    };
-
-    var errormsg={
-        "type": "bubble",
-        "header": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "故障訊息(連線／感測器)"
-            }
-          ]
-        },
-        "hero": {
-            "type": "image",
-            "url": "https://photos.app.goo.gl/YbnwnaCfCZFCGLEL9",
-        }            
-    };
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "角度狀態： "+ (data.ang=='0')?"Standing":"Lying",
+                },                
+                {
+                    "type": "text",
+                    "text": "是否拿著？ "+ (data.isactive=='0')?"沒拿":"拿著",
+                },
+                {
+                    "type": "text",
+                    "text": "本日腳步數： "+ stepcount +"步",
+                }                
+              ]
+            }            
+        };
     
-    if( data.alarm == 1){
-        msg.header.contents[0].text = "!!警示訊息!!";
-        msg.hero = {
-            "type": "image",
-            "url": "https://photos.app.goo.gl/j2hwSjuqgRFNPBhG9",
+        var errormsg={
+            "type": "bubble",
+            "header": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "故障訊息(連線／感測器)"
+                }
+              ]
+            },
+            "hero": {
+                "type": "image",
+                "url": "https://photos.app.goo.gl/YbnwnaCfCZFCGLEL9",
+            }            
+        };
+        
+        if( data.alarm == 1){
+            msg.header.contents[0].text = "!!警示訊息!!";
+            msg.hero = {
+                "type": "image",
+                "url": "https://photos.app.goo.gl/j2hwSjuqgRFNPBhG9",
+            }
         }
-    }
+    
+        var recpt=[msg];
+        if(data.error == 1){
+            recpt += errormsg;
+        };
+        let family_member = [];
 
-    var recpt=[msg];
-    if(data.error == 1){
-        recpt += errormsg;
-    };
-
-    var family = psql("SELECT line_id FROM ACCOUNTS WHERE dev_name=\'" + data.dev_name +"\';");
-    for(let member of family){        
-        pushmessage([recpt],family.line_id);
-    };
+        psql("SELECT line_id FROM ACCOUNTS WHERE dev_name=\'" + data.dev_name +"\';").then(family=>{
+                for(let member in family){
+                    family_member += member.line_id;
+                }
+                return family_member; 
+        })
+        .then( family_member =>{
+            console.log(family_member);
+            pushmessage([recpt],family_member);
+        });
+    });
 }
 
 function pushmessage(recpt,id){
@@ -369,7 +393,6 @@ function betteryschedule(){
     schedule.scheduledJob('0 00 * * * *',scanAccount);
     
     function scanAccount(){
-        var clients = psql("SELECT line_id,last_call_time FROM ACCOUNTS;");
         var date = new Date();
         var min  = date.getMinutes();
         var hour  = date.getHours();
@@ -377,58 +400,62 @@ function betteryschedule(){
         var month  = date.getMonth();
         var year = date.getFullYear();
 
-        for(let client of clients){
-            var clientmin=parseInt(JSON.parse(client.last_call_time).min);
-            var clienthour=parseInt(JSON.parse(client.last_call_time).hour);
-            var clientday=parseInt(JSON.parse(client.last_call_time).day);
-            var clientmonth=parseInt(JSON.parse(client.last_call_time).month);
-            var clientyear=parseInt(JSON.parse(client.last_call_time).year);
-            var alarm = false;
+        psql("SELECT line_id,last_call_time FROM ACCOUNTS;").then( clients =>{
 
-            if( ( (min - clientmin) < 30 && [clienthour,clientday,clientmonth,clientyear].toString()==[hour,day,month,year].toString() ) 
-            || ( (min==15) && ((75-clientmin) <30 ) && ( ( (hour-clienthour)==1 && [clientday,clientmonth,clientyear].toString()==[day,month,year].toString() )
-            || ((hour=0) && (clienthour == 23) && ( ( (day-clientday)==1 && [clientmonth,clientyear].toString()==[month,year].toString() )
-            || ((day==1) && (
-            (([clientday,clientmonth].toString() == [28,2].toString() 
-            || [clientday,clientmonth].toString() == [29,2].toString()
-            || [clientday,clientmonth].toString() == [31,1].toString()
-            || [clientday,clientmonth].toString() == [31,3].toString()
-            || [clientday,clientmonth].toString() == [30,4].toString()
-            || [clientday,clientmonth].toString() == [31,5].toString()
-            || [clientday,clientmonth].toString() == [30,6].toString()
-            || [clientday,clientmonth].toString() == [31,7].toString()
-            || [clientday,clientmonth].toString() == [31,8].toString()
-            || [clientday,clientmonth].toString() == [30,9].toString()
-            || [clientday,clientmonth].toString() == [31,10].toString()
-            || [clientday,clientmonth].toString() == [30,11].toString()
-            ) && clientyear==year)
-            ||
-            (
-                [clientday,clientmonth].toString() == [31,12].toString()
-                && (year-clientyear)==1
-            )
-            )))
-            )))){}
-            else{
-                var errormsg={
-                    "type": "bubble",
-                    "header": {
-                      "type": "box",
-                      "layout": "vertical",
-                      "contents": [
-                        {
-                          "type": "text",
-                          "text": "故障訊息(連線／沒電)"
-                        }
-                      ]
-                    },
-                    "hero": {
-                        "type": "image",
-                        "url": "https://photos.app.goo.gl/YbnwnaCfCZFCGLEL9",
-                    }            
-                };
-                pushmessage([recpt],client);
+            for(let client of clients){
+                var clientmin=parseInt(JSON.parse(client.last_call_time).min);
+                var clienthour=parseInt(JSON.parse(client.last_call_time).hour);
+                var clientday=parseInt(JSON.parse(client.last_call_time).day);
+                var clientmonth=parseInt(JSON.parse(client.last_call_time).month);
+                var clientyear=parseInt(JSON.parse(client.last_call_time).year);
+                var alarm = false;
+    
+                if( ( (min - clientmin) < 30 && [clienthour,clientday,clientmonth,clientyear].toString()==[hour,day,month,year].toString() ) 
+                || ( (min==15) && ((75-clientmin) <30 ) && ( ( (hour-clienthour)==1 && [clientday,clientmonth,clientyear].toString()==[day,month,year].toString() )
+                || ((hour=0) && (clienthour == 23) && ( ( (day-clientday)==1 && [clientmonth,clientyear].toString()==[month,year].toString() )
+                || ((day==1) && (
+                (([clientday,clientmonth].toString() == [28,2].toString() 
+                || [clientday,clientmonth].toString() == [29,2].toString()
+                || [clientday,clientmonth].toString() == [31,1].toString()
+                || [clientday,clientmonth].toString() == [31,3].toString()
+                || [clientday,clientmonth].toString() == [30,4].toString()
+                || [clientday,clientmonth].toString() == [31,5].toString()
+                || [clientday,clientmonth].toString() == [30,6].toString()
+                || [clientday,clientmonth].toString() == [31,7].toString()
+                || [clientday,clientmonth].toString() == [31,8].toString()
+                || [clientday,clientmonth].toString() == [30,9].toString()
+                || [clientday,clientmonth].toString() == [31,10].toString()
+                || [clientday,clientmonth].toString() == [30,11].toString()
+                ) && clientyear==year)
+                ||
+                (
+                    [clientday,clientmonth].toString() == [31,12].toString()
+                    && (year-clientyear)==1
+                )
+                )))
+                )))){}
+                else{
+                    var errormsg={
+                        "type": "bubble",
+                        "header": {
+                          "type": "box",
+                          "layout": "vertical",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "故障訊息(連線／沒電)"
+                            }
+                          ]
+                        },
+                        "hero": {
+                            "type": "image",
+                            "url": "https://photos.app.goo.gl/YbnwnaCfCZFCGLEL9",
+                        }            
+                    };
+                    pushmessage([recpt],client);
+                }
             }
-        }
+        });
+        
     }
 }
